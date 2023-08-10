e5a2ef52c088e16714f2492e41c4d6cb2f0f0a25
Gustavo Pinheiro <116049898+pinheiro22@users.noreply.github.com>
2023-08-09 22:16:49 -0300

    Add files via upload

---
+++ fundamentos/media.js
@@ -0,0 +1,33 @@
+/**
+ * App para calcular a m,édia de 2 notas
+ */
+
+//variaveis
+let nome, disciplina
+let nota1, nota2, media
+
+console.clear
+console.log("___.          .__          __  .__")         
+console.log("\\_ |__   ____ |  |   _____/  |_|__| _____")  
+console.log("| __ \\/  _ \\|  | _/ __ \\   __\\  |/     \\") 
+console.log("| \\_\\ (  <_> )  |_\\  ___/|  | |  |  Y Y  \\" )
+console.log("|___  /\\____/|____/\\___  >__| |__|__|_|  /")
+console.log("    \\/                 \\/              \\/ ")
+
+//entrada
+nome = "Gustavo"
+disciplina = "Matemática"
+nota1 = 9
+nota2 = 2
+
+//processamento
+media = (nota1 + nota2) / 2
+
+//saida
+console.log(`Nome: ${nome}`)
+console.log(`Disciplina: ${disciplina}`)
+console.log(`Nota1: ${nota1}`)
+console.log(`Nota2: ${nota2}`)
+console.log(`Média final: ${media}`)
+
+
