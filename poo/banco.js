0d029509217dd51e38ca2459317b002a48db1a2d
Gustavo Pinheiro <116049898+pinheiro22@users.noreply.github.com>
2023-10-16 22:11:55 -0300

    Add files via upload

--- poo/banco.js
+++ poo/banco.js
@@ -1,39 +1,67 @@
 /**
- * Exercicio de fixação POO
+ * Exercício de fixação - POO
  */
 
+//modelo
 class Conta {
-    constructor (numero, titular, saldo) {
+    //atributos
+    constructor(numero, titular, saldo) {
         this.numero = numero
         this.titular = titular
         this.saldo = saldo
     }
-
     //métodos
-    exibirSaldo() {
-        console.log(`Saldo: ${this.saldo. toFixed(2)}`)
+    exibirConta() {
+        console.log("________________________________")
+        console.log(`Titular: ${this.titular} Cc: ${this.numero}`)
     }
     exibirSaldo() {
-        console.log(`Saldo: ${this.saldo. toFixed(2)}`)
+        console.log(`Saldo: R$ ${this.saldo.toFixed(2)}`)
     }
     depositar(valor) {
         this.saldo += valor
-        console.log(`Credito de R$ ${valor.toFixed(2)}`)
+        console.log(`Crédito: R$ ${valor.toFixed(2)}`)
     }
     sacar(valor) {
         //validação
-        if(valor <= this.saldo) {
+        if (valor <= this.saldo) {
             this.saldo -= valor
-            console.log(`Débito de R$ ${valor.toFixed(2)}`)
+            console.log(`Débito: R$ ${valor.toFixed(2)}`)
         } else {
-            console.log("Saque não permitido")    
+            console.log("Saque não permitido")
+        }
+    }
+    pix(destinatario, valor) {
+        //validação
+        if (valor <= this.saldo) {
+            this.saldo -= valor //débito de um valor da conta
+            destinatario.saldo += valor //crédito do valor na conta destino
+            console.log(`PIX de R$ ${valor.toFixed(2)} para ${destinatario.titular}`)
+        } else {
+            console.log("Saldo insuficiente para transferência")
         }
     }
 }
 
-/**** Clientes ****/
+//herança
+class contaPoupanca extends Conta {
+    //atributos
+    constructor(numero, titular, saldo) {
+        super(numero, titular, saldo)
+    }
+    //métodos
+    exibirContaPoupanca() {
+        console.log("________________________________")
+        console.log(`Titular: ${this.titular} Cp: 500${this.numero}`)
+    }
 
-console.clear()                                                                                              
+    exibirSaldoPoupanca() {
+        console.log(`Saldo de poupança: R$ ${this.saldo.toFixed(2)}`)
+    }
+}
+
+/***** Clientes *****/
+console.clear()
 console.log("BBBBBBBBBBBBBBBBB               AAA               NNNNNNNN        NNNNNNNNKKKKKKKKK    KKKKKKK ")
 console.log("B::::::::::::::::B             A:::A              N:::::::N       N::::::NK:::::::K    K:::::K ")
 console.log("B::::::BBBBBB:::::B           A:::::A             N::::::::N      N::::::NK:::::::K    K:::::K ")
@@ -50,19 +78,37 @@ console.log("BB:::::BBBBBB::::::BA:::::A             A:::::A   N::::::N      N::
 console.log("B:::::::::::::::::BA:::::A               A:::::A  N::::::N       N:::::::NK:::::::K    K:::::K ")
 console.log("B::::::::::::::::BA:::::A                 A:::::A N::::::N        N::::::NK:::::::K    K:::::K ")
 console.log("BBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNNKKKKKKKKK    KKKKKKK ")
- 
-console.log(" ")
-//instanciar um objeto
-let cc1 = new Conta(1, "Leandro Ramos", 10000)
-console.log(`Clientes: ${cc1.titular} | conta: ${cc1.numero}`)
+
+//instanciar objetos
+let cc1 = new Conta(1000, "Leandro Ramos", 10000)
+let cc1p = new contaPoupanca(1000, "Leandro Ramos", 2000)
+let cc2 = new Conta(1001, "Sirlene Aparecida", 5000)
+
+//movimentação de conta corrente
+cc1.exibirConta()
 cc1.exibirSaldo()
-cc1.depositar(5000)
+cc1.sacar(2000)
 cc1.exibirSaldo()
-cc1.sacar(8000)
-cc1.exibirSaldo()
-cc1.sacar(8000)
+cc1.depositar(15000)
 cc1.exibirSaldo()
 
-let p1 = new Conta(10, "Leandro Ramos", 10000000)
-console.log(`Clientes: ${p1.titular}  | conta: ${p1.numero}`)
-p1.exibirSaldo()
+//conta poupança
+cc1p.exibirContaPoupanca()
+cc1p.exibirSaldoPoupanca()
+
+//conta corrente
+cc2.exibirConta()
+cc2.exibirSaldo()
+
+//pix (cc1 para cc2)
+cc1.exibirConta()
+cc1.exibirSaldo()
+cc1.pix(cc2, 1000)
+cc1.exibirSaldo()
+
+//conta corrente
+cc2.exibirConta()
+cc2.exibirSaldo()
+
+
+
